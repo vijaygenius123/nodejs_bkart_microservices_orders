4a1122b01dea56a6efca26cae46a25ecad79f651
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-25 21:44:27 +0530

    Added Create Order Method

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {Order} = require('./Order');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://orders_user:orders_password@cluster0.cbckc.mongodb.net/orders?retryWrites=true&w=majority",
+    {useUnifiedTopology: true},
+    () => {
+        console.log("DB Is Connected");
+    })
 
 app.use(bodyParser.json());
 
 app.get('/', ((req, res) => {
     res.send("Hello From Orders Microservice");
 }));
+
+
+app.post('/orders', (req, res) => {
+    const newOrder = {
+        customerId: mongoose.Types.ObjectId(req.body.customerId),
+        bookId: mongoose.Types.ObjectId(req.body.bookId),
+        createdAt: new Date()
+    }
+
+    const order = new Order(newOrder);
+
+    order.save()
+        .then((order) => {
+            console.log("Order Created");
+            res.json(order)
+        }).catch(err => {
+        throw err;
+    })
+
+})
 
 app.listen(4002, () => {
     console.log("Up & Running! Orders Microservice");
